87d0d3e168faf5093a03f207270abdbfc2b18966
mascai <msumoskalenko@gmail.com>
2019-09-13 00:37:18 +0300

    Add selection to form

--- cities/models.py
+++ cities/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
+    COUNTRY = (
+        ('RU', 'Russia'),
+        ('UKR', 'Ukraine'),
+        ('US', 'USA'),
+    )
+    category = models.CharField(max_length=100, choices=COUNTRY, default='RU')
 
     class Meta:
       verbose_name_plural = "cities"
